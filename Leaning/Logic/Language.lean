01bf31a857ad430e7824c8e007fe7276d3c9428f
Alastair Horn <alastairthorn@outlook.com>
2022-07-23 21:09:43 +0100

    Adds meaningless changes to Language.lean

--- Leaning/Logic/Language.lean
+++ Leaning/Logic/Language.lean
@@ -12,9 +12,28 @@ inductive Term (L : Language) where
   | var : String → Term L
   | funApp  : (f : L.Ω) → Vector (Term L) (L.funArity f)  → Term L
 
+namespace Term
+
+variable {L : Language}
+
+-- Function which given a term outputs the variables it used
+def vars : Term L → List String := sorry
+
+end Term
+
 inductive Formula (L : Language) where
   | falsehood : Formula L
   | equality : Term L → Term L → Formula L
   | predApp : (p : L.Pred) → Vector (Term L) (L.predArity p) → Formula L
   | impl : Formula L → Formula L → Formula L
   | forAll : String → Formula L → Formula L
+
+
+def isSentence (L : Language) : Formula L → Prop := sorry
+
+structure Sentence (L : Language) where
+  formula : Formula L
+  is_sentence : isSentence L formula
+
+-- A theory is defined by a set of sentences
+abbrev Theory (L : Language) := Set (Sentence L)
